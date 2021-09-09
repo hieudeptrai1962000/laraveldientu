a910420a273f850c88c71fc32978571a397a4ff4
hieudeptrai1962000 <mamthoi31@gmail.com>
2021-09-09 22:33:31 +0700

    dientu

---
+++ resources/views/back-end/home.blade.php
@@ -0,0 +1,83 @@
+@extends('back-end.layouts.master')
+@section('content')
+	<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">			
+		<div class="row">
+			<ol class="breadcrumb">
+				<li><a href="#"><svg class="glyph stroked home"><use xlink:href="#stroked-home"></use></svg></a></li>
+				<li class="active">Home</li>
+			</ol>
+		</div><!--/.row-->
+		<?php 
+          $oder = DB::table('oders')->count('*'); 
+          $oder_new = DB::table('oders')->whereRaw(" ( status=1 and type ='paypal') or (status=0 and type='cod')")->count(); 
+          $mem = DB::table('users')->count('*');            
+          $pro = DB::table('products')->count('*');            
+
+        ?>
+		<div class="row">
+			<div class="col-lg-12">
+				<h1 class="page-header">Dashboard</h1>
+			</div>
+		</div><!--/.row-->
+		
+		<div class="row">
+			<div class="col-xs-12 col-md-6 col-lg-3">
+				<div class="panel panel-blue panel-widget ">
+					<div class="row no-padding">
+						<div class="col-sm-3 col-lg-5 widget-left">
+							<svg class="glyph stroked app-window-with-content"><use xlink:href="#stroked-app-window-with-content"/></svg>
+						</div>
+						<div class="col-sm-9 col-lg-7 widget-right">
+							<div class="large">{!!$oder!!}</div>
+							<div class="text-muted"> tổng đơn hàng</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-12 col-md-6 col-lg-3">
+				<div class="panel panel-red panel-widget ">
+					<div class="row no-padding">
+						<div class="col-sm-3 col-lg-5 widget-left">
+							<svg class="glyph stroked app-window-with-content"><use xlink:href="#stroked-app-window-with-content"/></svg>
+						</div>
+						<div class="col-sm-9 col-lg-7 widget-right">
+							<div class="large">{!!$oder_new!!}</div>
+							<div class="text-muted"> Đơn hàng mới</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-12 col-md-6 col-lg-3">
+				<div class="panel panel-orange panel-widget">
+					<div class="row no-padding">
+						<div class="col-sm-3 col-lg-5 widget-left">
+							<svg class="glyph stroked bag"><use xlink:href="#stroked-bag"/></svg>
+						</div>
+						<div class="col-sm-9 col-lg-7 widget-right">
+							<div class="large">{!!$pro!!}</div>
+							<div class="text-muted">Sản phẩm</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-12 col-md-6 col-lg-3">
+				<div class="panel panel-teal panel-widget">
+					<div class="row no-padding">
+						<div class="col-sm-3 col-lg-5 widget-left">
+							<svg class="glyph stroked male-user"><use xlink:href="#stroked-male-user"></use></svg>
+						</div>
+						<div class="col-sm-9 col-lg-7 widget-right">
+							<div class="large">{!!$mem!!}</div>
+							<div class="text-muted">Khách hàng</div>
+						</div>
+					</div>
+				</div>
+			</div>		
+		</div><!--/.row-->							
+		<div class="row">
+			<div class="col-md-8">
+				<small>Coppyright 2021 from Hiếu</small>
+			</div>
+		</div>
+	</div>
+@endsection
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,28 @@
+    <div class="container-fluid">
+      <div class="container">
+        <hr>
+        <footer>
+          © <?=date("Y"); ?> Công Ty Cổ Phần của Hiếu ở Nam Định, trụ sở bên LonDon <br>
+          Thiết Kế Bởi : <a href="https://www.facebook.com/profile.php?id=100006402721909" title="" target="#">Hiếu</a>
+      </footer>
+      </div>
+    </div>
+    <a id="back-to-top" href="#" class="btn btn-warning btn-lg back-to-top" role="button" title="Click lên đâu trang" data-toggle="tooltip" data-placement="left"><span class="glyphicon glyphicon-chevron-up"></span></a>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="{!!url('public/bootstrap/js/bootstrap.min.js')!!}"></script>
+    {{-- <script src="{!!url('bootstrap/js/ie10-viewport-bug-workaround.js')!!}"></script> --}}
+    <script src="{!!url('public/js/menu.js')!!}"></script> 
+    <script type='text/javascript' src='{!!url('public/js/jquery.easing.1.3.js')!!}'></script> 
+    <script type='text/javascript' src='{!!url('public/js/camera.min.js')!!}'></script> 
+    <script type='text/javascript' src='{!!url('public/js/myscript.js')!!}'></script> 
+    <script type='text/javascript' src='{!!url('public/js/active-menu.js')!!}'></script> 
+    <script src="{!!url('public/js/bootstrap-image-gallery.min.js')!!}"></script>
+    <script src="{!!url('public/js/jquery.blueimp-gallery.min.js')!!}"></script>
+
+    {{-- validate --}}
+    <script src="{!!url('public/js/validate/jquery.validate.min.js')!!}"></script>
+    <script src="{!!url('public/js/validate/jquery.validate.js')!!}"></script>
+
+  </body>
+</html>
